700de1ff84eb41fbc5e85c2712efa55f7c5234a1
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2022-08-27 13:10:04 +0100

    wallet schematics

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { localDatabaseConfig } from '../ormconfig';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { localDatabaseConfig } from '../ormconfig';
     }),
     TypeOrmModule.forRoot(localDatabaseConfig),
     UserModule,
+    WalletModule,
   ],
   controllers: [AppController],
   providers: [AppService],
